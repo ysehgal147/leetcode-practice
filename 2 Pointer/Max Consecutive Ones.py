595914bf79dafb4ff8b369bc4e367b4ee70fe053
Yogesh Sehgal <yogeshsehgal99@rediff.com>
2020-07-31 17:05:08 +0530

    commit

---
+++ 2 Pointer/Max Consecutive Ones.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        arr = []
+        count = 0
+        ans = 0
+        for i in nums:
+            if i == 1:
+                count += 1
+                ans = max(ans, count)
+            else:
+                count = 0
+        return ans
